233044b0c97072575135eb6e389cba1da69a8a25
lenin fernando <ingeleninfernando@gmail.com>
2017-01-13 10:33:45 -0500

    restringe paginas privadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   root 'posts#index'
+  delete 'logout', to: 'sessions#destroy'
 
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  
+
 
  resources :users, only: [:new, :create]
 	
