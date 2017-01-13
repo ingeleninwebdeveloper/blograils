70ac671dc77cb54a45b69108501b0fefb8d7e4ea
lenin fernando <ingeleninfernando@gmail.com>
2017-01-13 10:11:59 -0500

    implementa autenticacion

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
+  root 'posts#index'
+
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  
+
  resources :users, only: [:new, :create]
-	root 'posts#index'
+	
 
 
 	get 'posts', to: 'posts#index'
