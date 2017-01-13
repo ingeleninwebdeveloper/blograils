1aa95abbc7120ba9398b1b32cdd1a6f93284ef78
lenin fernando <ingeleninfernando@gmail.com>
2017-01-13 08:14:21 -0500

    blog de rails

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+
+	def index
+
+		@posts = Post.all
+
+
+	end
+
+
+	def new
+
+		@post = Post.new
+
+
+	end
+
+	def create
+
+		@post = Post.new(product_params)
+  			if @post.save
+    		redirect_to posts_path, notice: "El producto fue publicado con éxito"
+ 			 else
+  		  render :new
+ 		end
+
+
+
+	end
+
+
+	private
+  def product_params
+    params.require(:post).permit(:body, :tittle)
+  end
+
+end
