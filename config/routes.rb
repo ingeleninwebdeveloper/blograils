119739c0180ced30bd1617320e56f768fa49829f
lenin fernando <ingeleninfernando@gmail.com>
2017-01-13 09:50:33 -0500

    usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+ resources :users, only: [:new, :create]
+	root 'posts#index'
+
 
 	get 'posts', to: 'posts#index'
 
@@ -8,7 +11,11 @@ Rails.application.routes.draw do
 	post '/posts', to: 'posts#create'
 
 	get '/posts/:id', to: 'posts#show', as: 'post'
-	get '/posts/:id/edit', to: 'posts#edit'
+
+	get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
+	patch '/posts/:id', to: 'posts#update'
+
+	delete '/posts/:id', to: 'posts#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
