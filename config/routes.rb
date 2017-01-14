188e24d0171ba83c3e0c1a18156cb190929d6f9f
lenin fernando <ingeleninfernando@gmail.com>
2017-01-13 23:01:19 -0500

    implementa funcionalidad comentarios

--- config/routes.rb
+++ config/routes.rb
@@ -11,20 +11,23 @@ Rails.application.routes.draw do
 
  resources :users, only: [:new, :create]
 	
+resources :posts do
 
+resources :comments, only: [:create]
+end
 
-	get 'posts', to: 'posts#index'
+	#get 'posts', to: 'posts#index'
 
-	get 'posts/new', to: 'posts#new', as: 'new_post'
+	#get 'posts/new', to: 'posts#new', as: 'new_post'
 
-	post '/posts', to: 'posts#create'
+	#post '/posts', to: 'posts#create'
 
-	get '/posts/:id', to: 'posts#show', as: 'post'
+	#get '/posts/:id', to: 'posts#show', as: 'post'
 
-	get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
-	patch '/posts/:id', to: 'posts#update'
+	#get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
+	#patch '/posts/:id', to: 'posts#update'
 
-	delete '/posts/:id', to: 'posts#destroy'
+	#delete '/posts/:id', to: 'posts#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
